5dd5bfb127bb6af797f4aed9fda8681ddb689ac4
AzrafAlmas <azraf.almas@gmail.com>
2025-07-12 21:20:47 -0400

    Import library added

--- documentUpload/views.py
+++ documentUpload/views.py
@@ -1,9 +1,16 @@
+
+
+# IMPORTS
+import import_library
+import os # Must be manually imported
 from django.shortcuts import render
 from django.http import HttpResponse
+
 
 # Create your views here.
 def aboutResponse(request):
     context_block = {}
+    print(os.getenv("mongo_login"))
     return render(request, "about.html", context_block)
 
 def uploadResponse(request):
