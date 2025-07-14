d9cd4b6e91d5b592f27bbf0cc8ff5894ce95cc15
AzrafAlmas <azraf.almas@gmail.com>
2025-07-14 19:30:22 -0400

    Added some admin model for phone number authentication

--- documentUpload/models.py
+++ documentUpload/models.py
@@ -9,12 +9,15 @@ from import_library_clone import *
 from pymongo import MongoClient
 import os
 
+
 # When a user is created, do this
 @receiver(post_save, sender=User)
 def user_created_handler(sender, instance, created, **kwargs):
 
     if created:  # Only run when user is created, not updated
         new_username = instance.username
+
+        UserProfile.objects.create(user=instance, phone_number="") # ADDS A NEW FIELD 
 
         # Connect to the mongodb database
         MONGODB_URL = os.getenv("mongo_login_FRONTEND")
@@ -50,4 +53,31 @@ def user_deleted_handler(sender, instance, **kwargs):
     print("Username to be Deleted: " + to_delete_username)
     
     users_collection.delete_one({"Username": to_delete_username})
+
+################## NEW ADMIN MODELS ##################
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = models.CharField(max_length=20, help_text="Enter phone number")
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.phone_number}"
+    
+## TO SYNC MONGODB AND DJANGO DATABASE STUFF ##
+@receiver(post_save, sender=UserProfile)
+def sync_phone_to_mongodb(sender, instance, **kwargs):
+    """Sync phone number changes to MongoDB"""
+    try:
+        MONGODB_URL = os.getenv("mongo_login_FRONTEND")
+        mongo_client = MongoClient(MONGODB_URL)
+        db = mongo_client["main"]
+        users_collection = db["users"]
         
+        # Update MongoDB with new phone number
+        users_collection.update_one(
+            {"Username": instance.user.username},
+            {"$set": {"Phone_Number": instance.phone_number}}
+        )
+        mongo_client.close()
+        print(f"Updated phone number for {instance.user.username} in MongoDB")
+    except Exception as e:
+        print(f"Error syncing phone to MongoDB: {e}")
