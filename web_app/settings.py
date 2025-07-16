7e5519e38ea3c7b41e62601ac120a4e252b5fdca
AzrafAlmas <azraf.almas@gmail.com>
2025-07-16 15:23:00 -0400

    Changed Debug=false

--- web_app/settings.py
+++ web_app/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qy1wp&o7cjk^q0pol5v+ux4-&!9sp7@5(*gmn!ub(#mx#$rblj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
